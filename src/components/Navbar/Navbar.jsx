7fd0a68765474106b1d0293000bad98a7d2152ef
rey <reynold.seq@gmail.com>
2022-04-30 04:03:16 +0530

    added navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,9 +1,17 @@
-
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
-  )
-}
+    <nav className="navbar">
+      <h1 className="navbar__heading">
+        <Link to="/">PIZZA CORNER</Link>
+      </h1>
+      <div className="navbar__links">
+        <Link to="/login">Login</Link>
+        <Link to="/register">Register</Link>
+      </div>
+    </nav>
+  );
+};
 
-export default Navbar
+export default Navbar;
