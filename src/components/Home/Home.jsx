38864616adf1eb339b40eec9bce6d0e64d327d36
rey <reynold.seq@gmail.com>
2022-04-30 04:19:14 +0530

    home page

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,5 +1,19 @@
+import { Link } from 'react-router-dom'
+
+
 const Home = () => {
-  return <div>Home</div>;
+  return <header className="header">
+      <div className="header__img">
+          <img src="" alt="pizza-img" className="header__pizza"/>
+      </div>
+      <div className="header__content">
+          <h1>Best Pizza In Every State</h1>
+          <h3>50% off on 1st Purchase</h3>
+          <Link to="/">
+            View Menu
+          </Link>
+      </div>
+  </header>;
 };
 
 export default Home;
