de0ea0b053a2933149777b8dc6364f0c95aba924
rey <reynold.seq@gmail.com>
2022-04-30 05:57:35 +0530

    added login register

--- src/routing/Login.jsx
+++ src/routing/Login.jsx
@@ -1,8 +1,16 @@
-
+import './Login.css'
 
 const Login = () => {
   return (
-    <div>Login</div>
+      <form className='login__form'>
+        <h1>Login Page</h1>
+        <label htmlFor="email">Email</label>
+        <input type="text" name="email" placeholder="Enter your email.." required/>
+        <label htmlFor="password">Password</label>
+        <input type="password" name="password" placeholder="password.." required/>
+        <button>Login</button>
+        <img src="/pizza2.png" alt="pizza-img" className='login__pizza'/>
+      </form>
   )
 }
 
--- src/routing/Register.jsx
+++ src/routing/Register.jsx
@@ -1,8 +1,20 @@
-
+import './Register.css'
 
 const Register = () => {
   return (
-    <div>Register</div>
+      <form className='register__form'>
+        <h1>Register Page</h1>
+        <label htmlFor="Email">Email</label>
+        <input type="text" name="email" placeholder="Enter your email.." required/>
+
+        <label htmlFor="password">Password</label>
+        <input type="password" name="password" placeholder="Password.." required/>
+
+        <label htmlFor="confirmpassword">ConfirmPassword</label>
+        <input type="confirmpassword" name="confirmpassword" placeholder="Password.." required/>
+        <button>Register</button>
+        <img src="/pizza2.png" alt="pizza-img" className='register__pizza'/>
+      </form>
   )
 }
 
